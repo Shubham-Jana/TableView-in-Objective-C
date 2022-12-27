41413dc7df2773a3a6ee9846e077261074c3fd2a
- shubham <shubham@eam360.com>
2022-12-28 01:47:29 +0530

    Completed Project.

--- TableView/ViewController.m
+++ TableView/ViewController.m
@@ -7,16 +7,66 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UITableViewDelegate, UITableViewDataSource>
+
+@property (nonatomic, retain) UITableView *tableView ;
 
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
-  [super viewDidLoad];
-  // Do any additional setup after loading the view.
+NSString *cellId = @"cellId";
+
+- (void)loadView {
+  [super loadView];
+  self.view = self.tableView ;
 }
 
+- (void)viewDidLoad {
+  [super viewDidLoad];
+  
+  self.navigationItem.title = @"Apple Event";
+  self.navigationController.navigationBar.prefersLargeTitles = YES;
+  
+  [self setupTableView];
+}
+
+-(void) setupTableView {
+  self.tableView = [[UITableView alloc] initWithFrame:CGRectZero style:UITableViewStyleInsetGrouped];
+  [self.tableView registerClass:UITableViewCell.class forCellReuseIdentifier:cellId];
+  [self.tableView setDelegate:self];
+  [self.tableView setDataSource:self];
+}
+
+- (nonnull UITableViewCell *)tableView:(nonnull UITableView *)tableView cellForRowAtIndexPath:(nonnull NSIndexPath *)indexPath {
+  UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellId forIndexPath:indexPath];
+  UIListContentConfiguration *content = [cell defaultContentConfiguration];
+  
+    // Configure content.
+  content.text = @"Ready. Set. Code.";
+  content.textProperties.font = [UIFont boldSystemFontOfSize:12];
+  content.textProperties.color = UIColor.secondaryLabelColor;
+  
+  content.secondaryText = @"#WWDC22";
+  content.secondaryTextProperties.font = [UIFont systemFontOfSize:14];
+  content.secondaryTextProperties.color = UIColor.labelColor;
+  
+  content.image = [UIImage systemImageNamed:@"hammer"];
+  
+  cell.contentConfiguration = content;
+  return cell;
+}
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+  return 5;
+}
+
+- (NSInteger)tableView:(nonnull UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+  return 5;
+}
+
+- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section {
+  return @"vids";
+}
 
 @end
