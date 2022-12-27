13e1acf1310c212087cb44ca6077886f07332a1a
- shubham <shubham@eam360.com>
2022-12-27 20:24:44 +0530

    Initial Commit

---
+++ TableView/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  TableView
+//
+//  Created by iOS Wizard on 27/12/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ TableView/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  TableView
+//
+//  Created by iOS Wizard on 27/12/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
